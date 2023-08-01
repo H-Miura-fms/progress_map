ee676c0fbcb4f19d6628436f70000c8c7f92bd29
H-miura <mmyha1289@gmail.com>
2023-08-01 18:37:46 +0900

    feat:titleを変更できるように

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -39,112 +39,131 @@ class _HomePageState extends State<HomePage> {
     double width = MediaQuery.of(context).size.width * 0.8;
     double height = width;
     double per = num / target;
+    final FocusNode targetNode = FocusNode();
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            const MarginVertical(size: 20),
-            const Text(
-              "target",
-              style: TextStyle(fontSize: 20),
-            ),
-            const MarginVertical(size: 20),
-            Container(
-              alignment: Alignment.center,
-              margin: const EdgeInsets.all(10),
-              // width: MediaQuery.of(context).size.width - 120,
-              // height: MediaQuery.of(context).size.width - 120,
-              decoration: const BoxDecoration(
-                  //color: Color.fromARGB(255, 240, 240, 240),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const MarginVertical(size: 20),
+              Padding(
+                padding: const EdgeInsets.only(left: 20, right: 20),
+                child: Focus(
+                  child: TextField(
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(fontSize: 20),
+                    controller: TextEditingController(text: "no title"),
+                    decoration: const InputDecoration(
+                      hintText: 'Enter a target!',
+                    ),
+                    focusNode: targetNode,
                   ),
-              child: Container(
-                width: width,
-                height: height,
-                decoration: const BoxDecoration(
-                    //  color: Color.fromARGB(255, 240, 240, 240),
-                    ),
-                child: Stack(
-                  alignment: AlignmentDirectional.center,
-                  children: [
-                    Text(
-                      "${(double.parse((double.parse(per.toStringAsFixed(3)) * 100).toStringAsFixed(2)))}%",
-                      style: TextStyle(
-                          fontSize: 40,
-                          color: (per >= 1)
-                              ? const Color.fromARGB(255, 255, 111, 0)
-                              : Colors.black),
-                    ),
-                    TweenAnimationBuilder<double>(
-                      tween: Tween<double>(begin: 0, end: (per > 1) ? 1 : per),
-                      curve: Curves.easeInOut,
-                      duration: const Duration(milliseconds: 1500),
-                      builder: (context, value, _) => SizedBox(
-                        width: width,
-                        height: height,
-                        child: CircularProgressIndicator(
-                          value: value,
-                          strokeWidth: 20,
-                          backgroundColor:
-                              const Color.fromARGB(255, 200, 200, 200),
-                        ),
-                      ),
-                    ),
-                  ],
+                  onFocusChange: (hasFocus) {
+                    if (!hasFocus) {
+                      targetNode.unfocus();
+                    }
+                  },
                 ),
               ),
-            ),
-            Container(
-              alignment: Alignment.center,
-              padding: const EdgeInsets.only(top: 30),
-              child: Text(
-                "${num}h / ${target}h",
-                style: const TextStyle(fontSize: 20),
-              ),
-            ),
-            TextButton(
-              onPressed: () async {
-                Picker(
-                    adapter: NumberPickerAdapter(data: [
-                      const NumberPickerColumn(
-                          begin: 0, end: 999, suffix: Text("h")),
-                      const NumberPickerColumn(
-                          begin: 0, end: 59, suffix: Text("min")),
-                    ]),
-                    delimiter: [
-                      PickerDelimiter(
-                          child: Container(
-                        width: 30.0,
-                        alignment: Alignment.center,
-                        child: const Icon(Symbols.go_to_line),
-                      ))
+              const MarginVertical(size: 20),
+              Container(
+                alignment: Alignment.center,
+                margin: const EdgeInsets.all(10),
+                // width: MediaQuery.of(context).size.width - 120,
+                // height: MediaQuery.of(context).size.width - 120,
+                decoration: const BoxDecoration(
+                    //color: Color.fromARGB(255, 240, 240, 240),
+                    ),
+                child: Container(
+                  width: width,
+                  height: height,
+                  decoration: const BoxDecoration(
+                      //  color: Color.fromARGB(255, 240, 240, 240),
+                      ),
+                  child: Stack(
+                    alignment: AlignmentDirectional.center,
+                    children: [
+                      Text(
+                        "${(double.parse((double.parse(per.toStringAsFixed(3)) * 100).toStringAsFixed(2)))}%",
+                        style: TextStyle(
+                            fontSize: 40,
+                            color: (per >= 1)
+                                ? const Color.fromARGB(255, 255, 111, 0)
+                                : Colors.black),
+                      ),
+                      TweenAnimationBuilder<double>(
+                        tween:
+                            Tween<double>(begin: 0, end: (per > 1) ? 1 : per),
+                        curve: Curves.easeInOut,
+                        duration: const Duration(milliseconds: 1500),
+                        builder: (context, value, _) => SizedBox(
+                          width: width,
+                          height: height,
+                          child: CircularProgressIndicator(
+                            value: value,
+                            strokeWidth: 20,
+                            backgroundColor:
+                                const Color.fromARGB(255, 200, 200, 200),
+                          ),
+                        ),
+                      ),
                     ],
-                    hideHeader: true,
-                    title: const Text("Please Select"),
-                    onConfirm: (Picker picker, List<int> value) {
-                      double selectedHour = value[0].toDouble();
-                      double selectedMin = (value[1].toDouble()) / 60;
-                      double selectedNum = selectedHour + selectedMin;
+                  ),
+                ),
+              ),
+              Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.only(top: 30),
+                child: Text(
+                  "${num.toStringAsFixed(2)}h / ${target}h",
+                  style: const TextStyle(fontSize: 20),
+                ),
+              ),
+              TextButton(
+                onPressed: () async {
+                  Picker(
+                      adapter: NumberPickerAdapter(data: [
+                        const NumberPickerColumn(
+                            begin: 0, end: 999, suffix: Text("h")),
+                        const NumberPickerColumn(
+                            begin: 0, end: 59, suffix: Text("min")),
+                      ]),
+                      delimiter: [
+                        PickerDelimiter(
+                            child: Container(
+                          width: 30.0,
+                          alignment: Alignment.center,
+                          child: const Icon(Symbols.go_to_line),
+                        ))
+                      ],
+                      hideHeader: true,
+                      title: const Text("Please Select"),
+                      onConfirm: (Picker picker, List<int> value) {
+                        double selectedHour = value[0].toDouble();
+                        double selectedMin = (value[1].toDouble()) / 60;
+                        double selectedNum = selectedHour + selectedMin;
 
-                      setState(() {
-                        num += selectedNum;
-                        prefs.setDouble('counter', num);
-                      });
-                    }).showDialog(context);
-              },
-              child: const Text("test"),
-            ),
-            TextButton(
-              onPressed: () {
-                setState(
-                  () {
-                    num = 0;
-                    prefs.setDouble('counter', 0);
-                  },
-                );
-              },
-              child: const Text("reset"),
-            )
-          ],
+                        setState(() {
+                          num += selectedNum;
+                          prefs.setDouble('counter', num);
+                        });
+                      }).showDialog(context);
+                },
+                child: const Text("test"),
+              ),
+              TextButton(
+                onPressed: () {
+                  setState(
+                    () {
+                      num = 0;
+                      prefs.setDouble('counter', 0);
+                    },
+                  );
+                },
+                child: const Text("reset"),
+              )
+            ],
+          ),
         ),
       ),
     );
